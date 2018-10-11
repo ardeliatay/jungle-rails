8a65d027bf0fa769a8d82c8e3afd33b62c304e42
Ardelia Tay <ardeliatay@gmail.com>
2018-10-11 23:45:29 +0000

    admin authentification to see admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTHENTICATE_WITH_NAME'],
+  :password      => ENV['HTTP_BASIC_AUTHENTICATE_WITH_PASSWORD']
   def index
     @categories = Category.order(id: :desc).all
   end
