29e30b189073ee7d043707851a1fe51c10ad2980
Ardelia Tay <ardeliatay@gmail.com>
2018-10-11 01:44:11 +0000

    add new users route and sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,13 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-  end
-
-  namespace :admin do
     resources :categories, only: [:index, :new, :create]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
